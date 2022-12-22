fea92b40f15b4bd4867b9ba9acb0133bd2445e84
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2022-12-22 20:14:11 +0700

    Add react-router-dom and styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <BrowserRouter>
+        <header>
+          <Link to='/'>ilham.io</Link>
+          <nav>
+            <Link to='/'>Blog</Link>
+            <Link to='/'>About</Link>
+            <Link to='/'>Work</Link>
+          </nav>
+        </header>
+        <section>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </section>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+    return (
+        <>
+            <h2 style={{fontSize: '64px', fontFamily: 'Raleway'}}>Hi! I'm Ilham</h2>
+            <p style={{width: ''}}> Asyu. </p>
+        </>
+    )
+}
+
+export default Home;
